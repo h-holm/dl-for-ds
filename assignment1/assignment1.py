c059639e6426460c0e9b6974742c307833875e87
Henrik Holm <henholm@kth.se>
2020-03-24 01:00:34 +0100

    Added reading dataset utilities

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -36,69 +36,6 @@ def main():
     print(X_train)
     print(Y_train)
 
-    # X = (dataDict[b"data"] / 255).T
-    # y = dataDict[b"labels"]
-    # Y = (np.eye(10)[y]).T
-    #
-    # with open(filename, 'rb') as f:
-    #     dataDict = pickle.load(f, encoding='bytes')
-    #
-    #     X = (dataDict[b"data"] / 255).T
-    #     y = dataDict[b"labels"]
-    #     Y = (np.eye(10)[y]).T
-    #
-    # return X, Y, y
-    #
-    #
-    # data = {
-    #     'X_train': X_train,
-    #     'Y_train': Y_train,
-    #     'y_train': y_train,
-    #     'X_val': X_val,
-    #     'Y_val': Y_val,
-    #     'y_val': y_val,
-    #     'X_test': X_test,
-    #     'Y_test': Y_test,
-    #     'y_test': y_test
-    # }
-    #
-    # clf = Classifier(data, labels)
-    #
-    # lambdas = [0, 0, .1, 1]
-    # etas = [.1, .01, .01, .01]
-    #
-    # for i in range(4):
-    #     acc_train_set = []
-    #     acc_val_set = []
-    #     acc_test_set = []
-    #     for j in range(10):
-    #         acc_train, acc_val, acc_test = clf.mini_batch_gd(
-    #                 X_train,
-    #                 Y_train,
-    #                 labda=lambdas[i],
-    #                 eta=etas[i],
-    #                 verbose=False)
-    #
-    #         acc_train_set.append(acc_train)
-    #         acc_val_set.append(acc_val)
-    #         acc_test_set.append(acc_test)
-    #     print("Settting " + str(i) + ":\n")
-    #     print("Train mean acc:" + str(statistics.mean(acc_train_set)))
-    #     print("Val mean acc:" + str(statistics.mean(acc_val_set)))
-    #     print("Test mean acc:" + str(statistics.mean(acc_test_set)))
-    #     print("Train stdev acc:" + str(statistics.stdev(acc_train_set)))
-    #     print("Val stdev acc:" + str(statistics.stdev(acc_val_set)))
-    #     print("Test stdev acc:" + str(statistics.stdev(acc_test_set)))
-    #
-    #     np.random.seed(0)
-    #
-    #     # Param settings 1
-    #     clf.mini_batch_gd(X_train, Y_train, title=str(i) + "_cost_plot_test",
-    #             labda=lambdas[i], eta=etas[i], plot_performance=True)
-    #     clf.plot_learned_images(save=True, num=i)
-    #
-    # # Unit testing
-    # unittest.main()
 
 
 
