1b5868dff81b4a9c5231f7e9482af921b4b7fe57
Henrik Holm <henholm@kth.se>
2020-03-30 15:55:39 +0200

    Adding svm

--- assignment1/assignment1_bonus.py
+++ assignment1/assignment1_bonus.py
@@ -170,42 +170,150 @@ class SingleLayerNetwork():
 
 		return count / N
 
-	def compute_gradients(self, X_batch, Y_batch, our_lambda, SVM=False):
+	def compute_gradients_entropy_loss(self, X_batch, Y_batch, our_lambda):
+		N = X_batch.shape[1]
+		C = Y_batch.shape[0]
+		P_batch = self.evaluate_classifier(X_batch)
+
+		# As per the last slide of lecture 3.
+		G_batch = - (Y_batch - P_batch)
+		grad_W = (1 / N) * (G_batch @ X_batch.T) + (2 * our_lambda * self.W)
+
+		# No regularization term necessary.
+		grad_b = np.reshape((1 / N) * (G_batch @ np.ones(N)), (C, 1))
+
+		return grad_W, grad_b
+
+	def compute_gradients_SVM_loss(self, X_batch, Y_batch, our_lambda):
+		"""
+		Data of dimension D, C classes, and minibatches of N examples.
+
+		Inputs:
+		- W: A numpy array of shape (D, C) containing weights.				(K, d)
+		- X: A numpy array of shape (N, D) containing a minibatch of data.	(d, N)
+		- y: A numpy array of shape (N,) containing training labels; y[i] = c means
+		that X[i] has label c, where 0 <= c < C.
+		- our_lambda: (float) regularization strength
+
+		Returns a tuple of:
+		- loss as single float
+		- gradient with respect to weights W; an array of same shape as W
+		"""
+		# Inspiration drawn from the following link:
+		# https://mlxai.github.io/2017/01/06/vectorized-implementation-of-svm-loss-and-gradient-update.html
+
+		N = X_batch.shape[1]
+
+		# s = self.evaluate_classifier(X_batch)
+		# sc = s.T[np.arange(s.shape[1]), np.argmax(Y_batch, axis=0)].T
+		# print(s.shape)
+		# print(sc.shape)
+		#
+		# margins = np.maximum(0, s - np.asarray(sc) + 1)
+		# print(margins.shape)
+		# margins.T[np.arange(N), np.argmax(Y_batch, axis=0)] = 0
+		# print(margins.shape)
+		#
+		# mcsvm_loss = Y_batch.shape[0] * np.mean(np.sum(margins, axis=1))
+		#
+		# cost = 1/N * mcsvm_loss + 0.5 * our_lambda * np.sum(self.W**2)
+		#
+		# binaries = margins
+		# binaries[margins > 0] = 1
+		# binaries_row_sum = np.sum(binaries, axis=0)
+		#
+		# print()
+		# print(np.arange(N).shape)
+		# print(binaries.T[np.arange(N), np.argmax(Y_batch, axis=0)].shape)
+		# print(binaries_row_sum.T.shape)
+		#
+		# binaries.T[np.arange(N), np.argmax(Y_batch, axis=0)] = -binaries_row_sum.T
+		# print(binaries.shape)
+		# print(binaries.T.shape)
+		#
+		# print(X_batch.T.shape)
+		# print(self.W.shape)
+		# grad_W = np.dot(binaries, X_batch.T) / N + our_lambda * self.W
+		# print(grad_W.shape)
+		#
+		# grad_b = np.reshape(np.sum(binaries, axis=1) / binaries.shape[1], self.b.shape)
+
+
+		loss = 0.0
+		grad_W = np.zeros(self.W.shape) # initialize the gradient as zero
+
+		# scores = X_batch.dot(self.W)
+		# yi_scores = scores[np.arange(scores.shape[0]),y] # http://stackoverflow.com/a/23435843/459241
+		scores = self.evaluate_classifier(X_batch)
+		print(scores.shape)
+		yi_scores = scores.T[np.arange(scores.shape[1]), np.argmax(Y_batch, axis=0)].T
+		print(yi_scores.shape)
+
+		# margins = np.maximum(0, scores - np.matrix(yi_scores).T + 1)
+		margins = np.maximum(0, scores - np.asarray(yi_scores) + 1)
+		print(margins.shape)
+		# margins = np.maximum(0, scores - np.matrix(yi_scores) + 1)
+		# margins = np.maximum(0, scores - yi_scores + 1)
+		# margins[np.arange(N),Y_batch] = 0
+		margins.T[np.arange(N), np.argmax(Y_batch, axis=0)] = 0
+		print(margins.shape)
+
+		loss = np.mean(np.sum(margins, axis=1))
+		loss += 0.5 * our_lambda * np.sum(self.W * self.W)
+		cost = (1 / N) * loss
+
+		binary = margins
+		binary[margins > 0] = 1
+		# row_sum = np.sum(binary, axis=1)
+		row_sum = np.sum(binary, axis=0)
+		# binary[np.arange(N), Y_batch] = -row_sum.T
+		print(np.arange(N).shape)
+		print(binary.T[np.arange(N), np.argmax(Y_batch, axis=0)].shape)
+		print(row_sum.T.shape)
+		binary.T[np.arange(N), np.argmax(Y_batch, axis=0)] = -row_sum.T
+
+		print(binary.shape)
+		print(binary.T.shape)
+
+		print(X_batch.T.shape)
+		print(self.W.shape)
+
+		grad_W = np.dot(binary, X_batch.T) / N + our_lambda * self.W
+		# grad_W = (np.dot(X_batch.T, binary) / N) + (our_lambda * self.W)
+		print(grad_W.shape)
+
+		grad_b = np.reshape(np.sum(binary, axis=1) / binary.shape[1], self.b.shape)
+
+		quit()
+
+		return grad_W, grad_b
+
+	def compute_gradients(self, X_batch, Y_batch, our_lambda):
 		""" Compute gradients of the weight and bias.
 			- X_batch is a D x N matrix
 			- Y_batch is a C x N one-hot-encoding vector
 			- our_lambda is the regularization term ("lambda" is reserved).
 			Returns the gradients of the weight and bias. """
-		N = X_batch.shape[1]
-		C = Y_batch.shape[0]
-
 		if self.SVM_loss:
-			grad_W = np.zeros((C, X_batch.shape[0]))
-			grad_b = np.zeros((C, 1))
-			for i in range(N):
-				x = X_batch[:, i]
-				y_int = np.where(Y_batch[:, [i]].T[0] == 1)[0][0]
-				s = np.dot(self.W, X_batch[:, [i]]) + self.b
-				for j in range(C):
-					if j != y_int:
-						if max(0, s[j] - s[y_int] + 1) != 0:
-							grad_W[j] += x
-							grad_W[y_int] -= x
-							grad_b[j, 0] += 1
-							grad_b[y_int, 0] -= 1
-			grad_W /= N
-			grad_W += (our_lambda * self.W)
-			grad_b /= N
+			grad_W, grad_b = self.compute_gradients_SVM_loss(X_batch, Y_batch, our_lambda)
+			# grad_W = np.zeros((C, X_batch.shape[0]))
+			# grad_b = np.zeros((C, 1))
+			# for i in range(N):
+			# 	x = X_batch[:, i]
+			# 	y_int = np.where(Y_batch[:, [i]].T[0] == 1)[0][0]
+			# 	s = np.dot(self.W, X_batch[:, [i]]) + self.b
+			# 	for j in range(C):
+			# 		if j != y_int:
+			# 			if max(0, s[j] - s[y_int] + 1) != 0:
+			# 				grad_W[j] += x
+			# 				grad_W[y_int] -= x
+			# 				grad_b[j, 0] += 1
+			# 				grad_b[y_int, 0] -= 1
+			# grad_W /= N
+			# grad_W += (our_lambda * self.W)
+			# grad_b /= N
 		else:
-			P_batch = self.evaluate_classifier(X_batch)
-
-			# As per the last slide of lecture 3.
-			G_batch = - (Y_batch - P_batch)
-
-			grad_W = (1 / N) * (G_batch @ X_batch.T) + (2 * our_lambda * self.W)
-
-			# No regularization term necessary.
-			grad_b = np.reshape((1 / N) * (G_batch @ np.ones(N)), (C, 1))
+			grad_W, grad_b = self.compute_gradients_entropy_loss(X_batch, Y_batch, our_lambda)
 
 		return grad_W, grad_b
 
@@ -337,7 +445,7 @@ def main():
 	tracc = accuracies["train"]
 	vacc = accuracies["val"]
 	teacc = accuracies["test"]
-	title = f'lambda{our_lambda}_n-batch{n_batch}_eta{eta}_n-epochs{n_epochs}_df-{decay_factor}_xavier-{xavier_str}_svm-{svm_str}_tr-acc{tracc}_v-acc{vacc}_te-acc{teacc}_seed{seed}'
+	title = f'lambda{our_lambda}_n-batch{n_batch}_eta{eta}_n-epochs{n_epochs}_df-{decay_factor}_xavier-{xavier_str}_svm-{SVM_str}_tr-acc{tracc}_v-acc{vacc}_te-acc{teacc}_seed{seed}'
 	plot_lines(line_A=costs['train'], line_B=costs['val'],
 			   label_A='training loss', label_B='validation loss',
 			   xlabel='epoch', ylabel='loss', title=title)
